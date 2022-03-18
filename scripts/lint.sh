b65f9d6199255ad7e91724209c33d46633d67924
pengpeidong <ppd0705@icloud.com>
2022-03-18 20:38:57 +0800

    feat(script/lint): upgrade shortcut usage

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -9,6 +9,6 @@ export SOURCE_FILES="supervisor_gateway tests"
 set -x
 
 ${PREFIX}autoflake --in-place --recursive $SOURCE_FILES
-${PREFIX}isort -sl --project=supervisor_gateway $SOURCE_FILES
+${PREFIX}isort --sl --project=supervisor_gateway $SOURCE_FILES
 ${PREFIX}black $SOURCE_FILES
 #${PREFIX}vulture $SOURCE_FILES --min-confidence 70
